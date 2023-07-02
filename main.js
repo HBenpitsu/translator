9e271cb7bf748fa15c64085565a83f36126203a3
HBenpitsu <hbenpitsu15@gmail.com>
2023-07-02 14:19:28 +0900

    another initialization

---
+++ main.js
@@ -0,0 +1,14 @@
+const http=require('http');
+require('dotenv').configDotenv();
+
+const HOST = process.env.HOST;
+const PORT = process.env.PORT;
+
+const server=http.createServer((request,response)=>{
+    response.writeHead(200, {'Content_Type':'text/plain'})
+    response.end('successfully connected.')
+});
+
+server.listen(PORT, HOST, () => {
+    console.log(`started on ${HOST}:${PORT}`)
+})
