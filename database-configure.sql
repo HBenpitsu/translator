a63d61197a4c0f643f4f1b53039f70012cce71b6
HBenpitsu <hbenpitsu15@gmail.com>
2023-07-27 08:49:37 +0900

    change database structure

--- database-configure.sql
+++ database-configure.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE discord_translator_bot;
 USE discord_translator_bot;
-CREATE TABLE messages (original_chl_id BIGINT UNSIGNED, original_msg_id BIGINT UNSIGNED,original_lang CHAR(2),translated_msg_id BIGINT UNSIGNED,translated_lang CHAR(2), expiration DATE);
+CREATE TABLE messages (bunch_id INT,chl_id BIGINT UNSIGNED, msg_id BIGINT UNSIGNED, lang CHAR(2), original BOOLEAN, expiration DATE);
+CREATE TABLE channels (bunch_id INT,chl_id BIGINT UNSIGNED,lang CHAR(2));
 SET GLOBAL event_scheduler=ON;
 CREATE EVENT clean_messages_table ON SCHEDULE EVERY 1 DAY DO DELETE FROM messages WHERE expiration < CURRENT_DATE();
 CREATE USER 'discord-translator-bot'@'localhost' IDENTIFIED BY 'password';
